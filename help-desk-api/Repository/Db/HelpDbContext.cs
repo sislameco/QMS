2721dfc08076804145c36fe6ac25fa1d8ca29e97
saiful.islam <saiful.islam@efficientsoftwaresolutions.com>
2025-09-08 14:53:04 +0600

    role permission

--- help-desk-api/Repository/Db/HelpDbContext.cs
+++ help-desk-api/Repository/Db/HelpDbContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Models.Entities.UserManagement;
+
 
 
 namespace Repository.Db
@@ -7,20 +7,20 @@ namespace Repository.Db
     public class HelpDbContext : DbContext
     {
         public HelpDbContext(DbContextOptions<HelpDbContext> options) : base(options) { }
-        public DbSet<UserModel> Users => Set<UserModel>();
-        public DbSet<RoleModel> Roles => Set<RoleModel>();
-        public DbSet<MenuModel> Menus => Set<MenuModel>();
-        public DbSet<UserRoleModel> UserRoles => Set<UserRoleModel>();
-        public DbSet<RoleMenuModel> RoleMenus => Set<RoleMenuModel>();
+        //public DbSet<UserModel> Users => Set<UserModel>();
+        //public DbSet<RoleModel> Roles => Set<RoleModel>();
+        //public DbSet<MenuModel> Menus => Set<MenuModel>();
+        //public DbSet<UserRoleModel> UserRoles => Set<UserRoleModel>();
+        //public DbSet<RoleMenuModel> RoleMenus => Set<RoleMenuModel>();
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<MenuModel>()
-                .HasOne(m => m.Parent)
-                .WithMany(p => p.Children)
-                .HasForeignKey(m => m.ParentId)
-                .OnDelete(DeleteBehavior.Restrict);
+            //modelBuilder.Entity<MenuModel>()
+            //    .HasOne(m => m.Parent)
+            //    .WithMany(p => p.Children)
+            //    .HasForeignKey(m => m.ParentId)
+            //    .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
