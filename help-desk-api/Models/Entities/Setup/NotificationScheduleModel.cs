1fdb9b016fc0701b50145c25a8f132b0db68bd79
saiful.islam <saiful.islam@efficientsoftwaresolutions.com>
2025-09-10 20:00:00 +0600

    Relation

--- help-desk-api/Models/Entities/Setup/NotificationScheduleModel.cs
+++ help-desk-api/Models/Entities/Setup/NotificationScheduleModel.cs
@@ -1,3 +1,5 @@
+using Models.Entities.Org;
+using Models.Enum;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,19 +8,25 @@ namespace Models.Entities.Setup
     [Table("NotificationSchedule", Schema = "setup")]
     public class NotificationScheduleModel : BaseEntity<long>
     {
-        public long NotificationConfigId { get; set; }
-        public int? TicketId { get; set; }
+        public long FKNotificationConfigId { get; set; }
+        public int? FKTicketId { get; set; }
         public string Recipient { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
         public NotificationType NotificationType { get; set; }
-        public long? EmailConfigurationId { get; set; }
-        public EmailConfigurationModel EmailConfiguration { get; set; }
+        public long? FKEmailConfigurationId { get; set; }
+
         public DateTime ScheduledTime { get; set; }
         public DateTime? SentTime { get; set; }
         public int RetryCount { get; set; }
         public int MaxRetryCount { get; set; }
         public NotificationScheduleStatus Status { get; set; }
         public string ErrorMessage { get; set; }
+
+        [ForeignKey("FKEmailConfigurationId")]
+        public EmailConfigurationModel EmailConfiguration { get; set; }
+
+        [ForeignKey("FKTicketId")]
+        public EmailConfigurationModel EmailConfiguration { get; set; }
     }
 }
