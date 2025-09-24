d4fc5a0a58494a44d40af742e24acae50b2145d3
saiful.islam <saiful.islam@efficientsoftwaresolutions.com>
2025-09-24 09:57:58 +0600

    menu action map

---
+++ help-desk-api/Repository/Migrations/20250923173339_test.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApiUrl",
+                schema: "UserMgmt",
+                table: "MenuActionMap",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoutePath",
+                schema: "UserMgmt",
+                table: "MenuActionMap",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoutePath",
+                schema: "UserMgmt",
+                table: "MenuActionMap");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApiUrl",
+                schema: "UserMgmt",
+                table: "MenuActionMap",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
