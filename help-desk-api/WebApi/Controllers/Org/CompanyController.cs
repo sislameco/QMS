ba405b993ea09bef795c11d08282b362e1db789c
saiful.islam <saiful.islam@efficientsoftwaresolutions.com>
2025-10-04 19:27:17 +0600

    merge

---
+++ help-desk-api/WebApi/Controllers/Org/CompanyController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Models.Dto.Org;
+using Services.Dashboard;
+using Services.Org;
+using System.Threading.Tasks;
+using WebApi.Helper.Security;
+
+namespace WebApi.Controllers.Org
+{
+    [ApiController]
+    [Route("company")]
+    [CustomAuthorization]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICompanyService _companyService;
+        public CompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
+        [HttpGet("active")]
+        public async Task<IActionResult> GetActiveCompanies()
+        {
+            var companies = await _companyService.GetActiveCompaniesAsync();
+            return Ok(companies);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCompany(int id, [FromBody] CompanyDto dto)
+        {
+            return Ok(await _companyService.UpdateCompanyAsync(id, dto));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCompany(int id)
+        {
+            return Ok(await _companyService.GetCompany(id));
+        }
+    }
+}
