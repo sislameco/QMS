f9db2e5b9e91a7b93c3af0882479d15e34ead9b1
saiful.islam <saiful.islam@efficientsoftwaresolutions.com>
2025-10-27 15:25:00 +0600

    added column in users

--- help-desk-api/Repository/Migrations/20251027092417_AlterUser.Designer.cs
+++ help-desk-api/Repository/Migrations/20251027092417_AlterUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Repository.Db;
@@ -11,9 +12,11 @@ using Repository.Db;
 namespace Repository.Migrations
 {
     [DbContext(typeof(HelpDbContext))]
-    partial class HelpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251027092417_AlterUser")]
+    partial class AlterUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ help-desk-api/Repository/Migrations/20251027092417_AlterUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTenantAdmin",
+                schema: "UserMgmt",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTenantAdmin",
+                schema: "UserMgmt",
+                table: "Users");
+        }
+    }
+}
